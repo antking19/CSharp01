2f21837c4d720e2b6de89132840e5213e622006e
antking19 <cuong.truongkien@gmail.com>
2019-09-25 10:03:58 +0700

    Commit Ex01+Ex02

--- Ex01/Ex01/Program.cs
+++ Ex01/Ex01/Program.cs
@@ -13,8 +13,9 @@ namespace Ex01
             int countChan = 0;
             int countLe = 0;
 
-            for (int i = 0; i<phantu.Length; i++) {
-                if(phantu[i] % 2 == 0)
+            for (int i = 0; i < phantu.Length; i++)
+            {
+                if (phantu[i] % 2 == 0)
                 {
                     Console.WriteLine("So chan: {0}", phantu[i]);
                     countChan++;
@@ -32,6 +33,25 @@ namespace Ex01
                 }
             }
             Console.WriteLine("Co tat ca {0} so le", countLe);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("==========================EX02=================================");
+
+            int[] phantu2 = { 22, 252, 5, 36, 6, 3, 63, 6, 36 };
+            int value = 6;
+            findPosition(value, phantu2);
+
+        }
+        public static void findPosition(int x, int[] arr)
+        {
+            for (int i = 0; i<arr.Length; i++)
+            {               
+                if(arr[i] / x == 1)
+                {
+                    Console.WriteLine("Phan tu X trong mang o vi tri thu {0}", i+1 );
+                }
+            }
         }
     }
 }
